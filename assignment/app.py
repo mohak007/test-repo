26ca0c07c4fbeec9f02e5abbd92bf021bd6fa344
Mohak kataria <mohak.kumar53@gmail.com>
2026-03-15 13:29:52 +0530

    added todo html

--- assignment/app.py
+++ assignment/app.py
@@ -1,3 +1,5 @@
+from unittest import result
+
 from flask import Flask, render_template, request
 import os
 import json
@@ -11,6 +13,7 @@ MONGO_URI = os.getenv('MONGO_URI')
 client = pymongo.MongoClient(MONGO_URI)
 db=client.test
 collection=db['flask-tutorial-assignment']
+print("Mongo URI:", MONGO_URI)
 
 app = Flask(__name__)
 @app.route('/api', methods=['GET'])
@@ -38,6 +41,27 @@ def todo():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	return render_template('login.html')
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo_item():
+
+    data = request.get_json()
+    print("Received data:", data)
+
+    todo_data = {
+        "itemName": data.get("itemName"),
+        "itemDescription": data.get("itemDescription"),
+        "itemID": data.get("itemID"),
+        "itemUUID": data.get("itemUUID"),
+        "itemHash": data.get("itemHash")
+    }
+
+    result = collection.insert_one(todo_data)
+    print("Mongo URI:", MONGO_URI) 
+    print("Inserted ID:", result.inserted_id)
+
+    return {"message": "Todo stored successfully"}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
